dcc545a4cd169cb93c10c44bb887ea44ac9d882f
Suzume <suzume.logs@gmail.com>
2024-09-14 18:25:39 +0700

    Update

--- src/libs/components/Form/UploadImg/UploadImage.tsx
+++ src/libs/components/Form/UploadImg/UploadImage.tsx
@@ -1,7 +1,6 @@
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline'
 import { Box, IconButton, Typography } from '@mui/material'
 import { styled } from '@mui/material/styles'
-import Image from 'next/image' // Import Image từ next/image
 import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { useController } from 'react-hook-form'
@@ -104,7 +103,11 @@ const UploadImage = ({
           <DropzoneText variant="body2">Kéo và thả hình ảnh vào đây hoặc nhấp để chọn</DropzoneText>
         ) : (
           <ImagePreview>
-            <Image src={URL.createObjectURL(image)} alt="Preview" layout="fill" objectFit="cover" />
+            <img
+              src={URL.createObjectURL(image)}
+              alt="Preview"
+              style={{ width: '100%', height: 'auto' }}
+            />
             <RemoveButton color="error" onClick={handleRemoveImage}>
               <DeleteOutlineIcon />
             </RemoveButton>
