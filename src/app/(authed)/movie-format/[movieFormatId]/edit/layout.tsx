e720905e014f2dfba594d2844483fa3967550d9c
CuongThieu <cuongtt@solashi.com>
2024-09-23 17:38:54 +0700

    Update

--- src/app/(authed)/movie-format/[movieFormatId]/edit/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Movie format edit',
-}
-
-export default function Page({ children }: { children: React.ReactNode }) {
-  return children
-}
--- src/features/movie-format/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './useDeleteMovieFormat'
-export * from './useMovieFormatCreate'
-export * from './useMovieFormatDetail'
-export * from './useMovieFormatDetailQuery'
-export * from './useMovieFormatListQuery'
-export * from './useMovieFormatUpdate'
